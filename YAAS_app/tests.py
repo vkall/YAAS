b1923c18345e26657efcf6a97ad73f87af806b2e
vkall <vkall@abo.fi>
2013-10-31 20:55:23 +0200

    Tests for create auction

--- YAAS_app/tests.py
+++ YAAS_app/tests.py
@@ -6,11 +6,50 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+from YAAS_app.models import *
+from YAAS_app.forms import *
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MyBlogTest(TestCase):
+    def setUp(self):
+        self.username = "username"
+        self.password = "password"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.createUrl = "/YAAS/create_auction/"
+        self.confirmationUrl = "/YAAS/confirmation/"
+
+    def testAuthorizationCreateAuction(self):
+        # If the client is not logged in, create_user should give a redirect status (302)
+        response = self.client.get(self.createUrl)
+        self.assertEqual(response.status_code, 302)
+
+        # Login
+        loggedIn = self.client.login(username=self.username, password=self.password)
+        self.assertTrue(loggedIn)
+
+        # If the client is logged in, editblog should give a success status (200)
+        response = self.client.get(self.createUrl)
+        self.assertEqual(response.status_code, 200)
+
+    def testCreateAuction(self):
+        auctionData= {
+            "title": "title",
+            "description": "description",
+            "end_date": (timezone.now() + timedelta(days=4)).date(),
+            "minimum_price": 10.50,
+            "option": "Yes"
+        }
+
+        # Login
+        loggedIn = self.client.login(username=self.username, password=self.password)
+        self.assertTrue(loggedIn)
+
+        # 0 auctions in database
+        self.assertEqual(Auction.objects.all().count(), 0)
+
+        # create auction by posting data
+        self.client.post(self.confirmationUrl, auctionData)
+
+        # 1 auction in database
+        self.assertEqual(Auction.objects.all().count(), 1)
