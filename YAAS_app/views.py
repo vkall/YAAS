96db77bd5baffee6a61ce955548b9ae26efce7df
Viktor <vkall@abo.fi>
2013-11-02 11:08:46 +0200

    Winning bidder cant bid again

--- YAAS_app/views.py
+++ YAAS_app/views.py
@@ -144,17 +144,22 @@ def ban_auction(request, id):
 def bid_auction(request, id):
     auction = Auction.getActiveById(id)
     if auction:
+        latest_bid = Bid.getLatestBidForAuction(auction)
         if request.user.id == auction.seller.id:
             template = "message.html"
             context = {"message": "You can't bid on your own auctions!"}
             # Error: Seller tried to bid on own auction
             return render_to_response(template, context, context_instance=RequestContext(request))
+        elif request.user.id == latest_bid.bidder.id:
+            template = "message.html"
+            context = {"message": "You can't bid on an auction that you are already winning!"}
+            # Error: Winning bidder tried to bid again
+            return render_to_response(template, context, context_instance=RequestContext(request))
         else:
             if request.method == "POST":
                 form = BidForm(request.POST)
-
                 if (form.is_valid() and form.cleaned_data["bid"] > auction.minimum_price
-                        and form.cleaned_data["bid"] > auction.getLatestBid()):
+                        and form.cleaned_data["bid"] > latest_bid.bid):
 
                     bid = Bid()
                     bid.bid = form.cleaned_data["bid"]
