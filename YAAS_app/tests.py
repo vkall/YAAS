0561a974b4a0edd84256c9445423d1e5723e3929
Viktor <vkall@abo.fi>
2013-11-04 18:42:29 +0200

    Tests for bidding

--- YAAS_app/tests.py
+++ YAAS_app/tests.py
@@ -11,7 +11,7 @@ from YAAS_app.models import *
 from YAAS_app.forms import *
 
 
-class MyBlogTest(TestCase):
+class YAASTest(TestCase):
 
     fixtures = ['test_fixture.json']
 
@@ -20,6 +20,7 @@ class MyBlogTest(TestCase):
         self.user2 = "user2"
         self.createUrl = "/YAAS/auction/create/"
         self.confirmationUrl = "/YAAS/auction/create/confirmation/"
+        self.bidUrl = "/YAAS/auction/1/bid/"
 
     def testAuthorizationCreateAuction(self):
         # If the client is not logged in, create_user should give a redirect status (302)
@@ -51,3 +52,44 @@ class MyBlogTest(TestCase):
         self.client.post(self.confirmationUrl, auctionData)
         # auction count is 1 higher than before
         self.assertEqual(Auction.objects.all().count(), auctions+1)
+        self.client.logout()
+
+    def testUnauthorizedBid(self):
+        bid = 50.50
+        # If the client is not logged in, bid should give a redirect status (302)
+        response = self.client.post(self.bidUrl, {"bid": bid, "updated": timezone.now().date()})
+        self.assertEqual(response.status_code, 302)
+
+    def testSellerBid(self):
+        # If seller makes a bid on his own auction, bid should not be saved
+
+        bid = 50.50
+        # Login as user1 (seller)
+        loggedIn = self.client.login(username=self.user1, password=self.user1)
+        self.assertTrue(loggedIn)
+
+        auction = Auction.getActiveById(1)
+        self.assertEqual(self.client.session['_auth_user_id'], auction.seller.id)
+
+        bids = auction.getBidHistory().count()
+        self.client.post(self.bidUrl, {"bid": bid, "updated": timezone.now().date()})
+
+        self.assertEqual(auction.getBidHistory().count(), bids)
+        self.client.logout()
+
+    def testCorrectBid(self):
+        # Bid should be saved
+
+        bid = 50.50
+        # Login as user2
+        loggedIn = self.client.login(username=self.user2, password=self.user2)
+        self.assertTrue(loggedIn)
+
+        auction = Auction.getActiveById(1)
+        self.assertNotEqual(self.client.session['_auth_user_id'], auction.seller.id)
+        bids = auction.getBidHistory().count()
+
+        self.client.post(self.bidUrl, {"bid": bid, "updated": timezone.now().date()})
+
+        self.assertEqual(auction.getBidHistory().count(), bids+1)
+        self.client.logout()
